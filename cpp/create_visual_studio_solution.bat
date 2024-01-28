b8ddbd9ddd3db80fba9576d50ec960b392657db8
wassyworks <nasyorz@gmail.com>
2024-01-28 23:56:19 +0900

    - 最低限のVisualStudioビルド環境を構築

---
+++ cpp/create_visual_studio_solution.bat
@@ -0,0 +1,3 @@
+@echo off
+cmake -G "Visual Studio 17 2022" -T "v143" ".\"
+@pause
---
+++ cpp/examples/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// main function
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
