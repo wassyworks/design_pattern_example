bae70cf9af87d8cb543ded0812f3206beb46412e
wassyworks <nasyorz@gmail.com>
2024-02-04 23:17:58 +0900

    - CRTP実装パターン例を追加

--- cpp/examples/src/main.cpp
+++ cpp/examples/src/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 
+#include "crtp.h"
 // main function
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// === CRTP ===
+	const PlayerId player_1_id = PlayerId(1);
+	const PlayerId player_2_id = PlayerId(2);
+
+	// 計算はコンパイルエラーで未然に防止できる
+	// auto invalid_addition = player_1_id + player_2_id;
+
+	const PacketId packet_id = PacketId(100);
+	std::cout << "Packet Id:" << packet_id << std::endl;
+
+	Meter m1 = Meter(1);
+	const Meter m2 = Meter(2);
+	m1 += m2;
+	std::cout << "Meter Added:" << m1 + m2 << std::endl;
+
 	return 0;
 }
