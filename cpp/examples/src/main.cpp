7e5d67bccaee2ff34e385cbde4967c2c7b508f04
wassyworks <nasyorz@gmail.com>
2024-02-13 00:10:05 +0900

    - std::functionを使ったStrategyパターンのサンプル

--- cpp/examples/src/main.cpp
+++ cpp/examples/src/main.cpp
@@ -2,6 +2,9 @@
 
 #include "crtp.h"
 
+#include "strategy/player_update_strategy.h"
+#include "strategy/enemy_update_strategy.h"
+
 int main()
 {
 	// === CRTP ===
@@ -18,6 +21,17 @@ int main()
 	const Meter m2 = Meter(2);
 	m1 += m2;
 	std::cout << "Meter Added:" << m1 + m2 << std::endl;
+
+	// === Strategy ===
+	std::vector<std::unique_ptr<Entity>> entities;
+
+	entities.emplace_back(std::make_unique<Player>(PlayerUpdateStrategy()));
+	entities.emplace_back(std::make_unique<Enemy>(EnemyUpdateStrategy()));
+	entities.emplace_back(std::make_unique<Enemy>(EnemyUpdateStrategy()));
+	for (auto&& entity : entities)
+	{
+		entity->update();
+	}
 
 	return 0;
 }
