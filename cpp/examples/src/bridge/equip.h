e93f7b127012cf033f4ff328c7dc1bbf0b576b00
wassyworks <nasyorz@gmail.com>
2025-07-05 22:56:35 +0900

    EquipクラスにEquipStatusを追加し、装備の攻撃力と防御力を管理

--- cpp/examples/src/bridge/equip.h
+++ cpp/examples/src/bridge/equip.h
@@ -2,7 +2,19 @@
 #define BRIGDE_EQUIP_H_
 
 namespace bridge {
+	class EquipStatus {
+	public:
+		explicit EquipStatus(uint64_t attack, uint64_t defense) : attack_(attack), defense_(defense) {}
+	private:
+		uint64_t attack_ = 0;
+		uint64_t defense_ = 0;
+	};
 	class Equip {
+	public:
+		virtual ~Equip() = default;
+		const EquipStatus& GetEquipStatus() const { return equip_status_; }
+	private:
+		EquipStatus equip_status_;
 
 	};
 }
